2bf069f1037e8975a890e812516cbe5ea3b95835
naiza2000 <58358790+naiza2000@users.noreply.github.com>
2020-03-15 23:54:07 +0530

    Update Mutations.py

--- Mutations.py
+++ Mutations.py
@@ -3,3 +3,8 @@ def mutate_string(string, position, character):
     l[position] = character
     string = ''.join(l)
     return string
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
